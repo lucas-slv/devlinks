fceacade3a0f61ae02bb19f8162726023210d1f7
Lucas Mendes <lucasms.540@gmail.com>
2023-10-07 22:22:45 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode(){
+  const html = document.documentElement
+  html.classList.toggle('light')
+  
+  const img = document.querySelector('#profile img')
+  if(html.classList.contains('light')){
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('alt', 'imagem do bart (personagem de simpsons)')
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "imagem do rick de (rick and morty)")
+  }
+}
